8a50235b342e20ad7589eacbb2d59e29e4c6a2ea
JMante1 <40006518+JMante1@users.noreply.github.com>
2022-02-22 09:37:13 -0700

    addgene analysis half done

--- annotation_percentage.py
+++ annotation_percentage.py
@@ -9,7 +9,7 @@ file_list = os.listdir(os.path.join(cwd, 'synbict_output'))
 out_dict = {}
 
 for ind, file_name in enumerate(file_list):
-    print(file_name, f'{ind} of {len(file_list)}')
+    print(file_name, f'{ind} of {len(file_list)} is {ind/len(file_list)}')
     num_annotated = 0
     annotation_numbers = []
     doc_read = sbol2.Document()
@@ -19,7 +19,7 @@ for ind, file_name in enumerate(file_list):
             annotation_numbers.append(len(defin.sequenceAnnotations))
             num_annotated +=1
     out_dict[file_name] = [file_name, len(doc_read.componentDefinitions), num_annotated, annotation_numbers]
-    print(out_dict[file_name])
-print(out_dict)
+    # print(out_dict[file_name])
+# print(out_dict)
 with open(os.path.join(cwd, 'Analysis', 'annotation_number.txt'), 'w') as file:
      file.write(json.dumps(out_dict))
