857f7c578c82e0bf9d67598d94ac740dbefaf4d6
JMante1 <40006518+JMante1@users.noreply.github.com>
2022-03-21 18:51:11 -0600

    no snap

--- annotation_percentage.py
+++ annotation_percentage.py
@@ -9,17 +9,21 @@ file_list = os.listdir(os.path.join(cwd, 'synbict_output'))
 out_dict = {}
 
 for ind, file_name in enumerate(file_list):
-    print(file_name, f'{ind} of {len(file_list)} is {ind/len(file_list)}')
     num_annotated = 0
     annotation_numbers = []
     doc_read = sbol2.Document()
     doc_read.read(os.path.join(cwd, 'synbict_output', file_name))
     for defin in doc_read.componentDefinitions:
         if len(defin.sequenceAnnotations) > 0:
-            annotation_numbers.append(len(defin.sequenceAnnotations))
+            an_num = 0
+            for an in defin.sequenceAnnotations:
+                if an.component is not None:
+                    an_num += 1
+            annotation_numbers.append(an_num)
             num_annotated +=1
     out_dict[file_name] = [file_name, len(doc_read.componentDefinitions), num_annotated, annotation_numbers]
+    print(file_name, f'{ind} of {len(file_list)} is {ind/len(file_list)} with {an_num} component annotations')
     # print(out_dict[file_name])
 # print(out_dict)
-with open(os.path.join(cwd, 'Analysis', 'annotation_number.txt'), 'w') as file:
+with open(os.path.join(cwd, 'Analysis', 'annotation_number_nosnap.txt'), 'w') as file:
      file.write(json.dumps(out_dict))
