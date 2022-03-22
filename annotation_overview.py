fd26b0a14fe5cea20a4d9897510e045a3960a8eb
JMante1 <40006518+JMante1@users.noreply.github.com>
2022-03-21 18:13:15 -0600

    no snap analysis

--- annotation_overview.py
+++ annotation_overview.py
@@ -24,23 +24,24 @@ for ind, file in enumerate(file_list):
             len_seq = len(sequence)
             # print (comp)
             for an in comp.sequenceAnnotations:
-                for loc in an.locations:
-                    an_seq = sequence.elements[loc.start-1:loc.end-1]
-                    percent_cover = '{:.2f}'.format(len(an_seq)/ (len_seq-1))
-                    an_dict[an_seq] = {'an_name':an.name, 'an_len':len(an_seq), 'percent_cover':[percent_cover]}
-                    if an_seq in all_an_dict:
-                        all_an_dict[an_seq]['count'] +=1
-                        all_an_dict[an_seq]['percent_cover'].append(percent_cover)
-                    else:
-                        all_an_dict[an_seq] = {'an_name':an.name, 'an_len':len(an_seq), 'count':1, 'percent_cover':[percent_cover]}
-                    # print(an, loc.start, loc.end, an.name)
+                if an.component is not None:
+                    for loc in an.locations:
+                        an_seq = sequence.elements[loc.start-1:loc.end-1]
+                        percent_cover = '{:.2f}'.format(len(an_seq)/ (len_seq-1))
+                        an_dict[an_seq] = {'an_name':an.name, 'an_len':len(an_seq), 'percent_cover':[percent_cover]}
+                        if an_seq in all_an_dict:
+                            all_an_dict[an_seq]['count'] +=1
+                            all_an_dict[an_seq]['percent_cover'].append(percent_cover)
+                        else:
+                            all_an_dict[an_seq] = {'an_name':an.name, 'an_len':len(an_seq), 'count':1, 'percent_cover':[percent_cover]}
+                        # print(an, loc.start, loc.end, an.name)
             
             components_dict[file]={'num_annotations':len(comp.sequenceAnnotations), 'annotation_dict':an_dict}
             # print(all_dict)
 # print(components_dict)
 df = pd.DataFrame.from_dict(components_dict, orient='index')
-df.to_csv(os.path.join(cwd, 'Analysis', 'subparts_overview.csv'))
-df.to_excel(os.path.join(cwd, 'Analysis', 'subparts_overview.xlsx'))
+df.to_csv(os.path.join(cwd, 'Analysis', 'subparts_overview_nosnap1.csv'))
+df.to_excel(os.path.join(cwd, 'Analysis', 'subparts_overview_nosnap1.xlsx'))
 
 df1 = pd.DataFrame.from_dict(all_an_dict, orient='index')
-df1.to_excel(os.path.join(cwd, 'Analysis', 'annotations_set.xlsx'))
+df1.to_excel(os.path.join(cwd, 'Analysis', 'annotations_set_nosnap1.xlsx'))
